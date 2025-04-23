f2979c9594ace6cccd3156981d7778446eb9927b
MaxPowerBoy1980 <124570842+MaxPowerBoy1980@users.noreply.github.com>
2025-04-23 21:32:38 +0200

    Update Script2.sh: refined logging and user detection

--- generic/Script2.sh
+++ generic/Script2.sh
@@ -13,27 +13,13 @@ echo "User: $(whoami)"
 echo "Terminal: $TERM"
 echo "PATH: $PATH"
 
-# logged_in_user=$(stat -f "%Su" /dev/console)
-# user_name=$(dscl . -read /Users/"$logged_in_user" RealName | tail -1)
-# first_name=$(echo "$user_name" | awk '{print $1}')
-# echo "👋 Hello, $user_name!"
+logged_in_user=$(stat -f "%Su" /dev/console)
+user_name=$(dscl . -read /Users/"$logged_in_user" RealName | tail -1)
+first_name=$(echo "$user_name" | awk '{print $1}')
+echo "👋 Hello, $user_name!"
 
 # Get model and serial
-# model_id=$(sysctl -n hw.model)
-# serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-
-# Construct personalized name
-# new_device_name="${first_name}-DTU-IT-${model_id}-${serial_number}"
-# echo "📛 Setting system names to: $new_device_name"
-
-# Create temporary command file
-# command_file=$(mktemp /var/tmp/dialog_command.XXXXXX)
-# chown "$logged_in_user" "$command_file"
-# : >"$command_file" # Clear it
-
-sleep 4
-sleep 10
+model_id=$(sysctl -n hw.model)
+serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 
 echo "---- Script ended at $(date) ----"
-
-exit 0
