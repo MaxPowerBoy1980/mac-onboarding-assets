827779df3dd738e62e5f1db90d02ae75003881d3
MaxPowerBoy1980 <124570842+MaxPowerBoy1980@users.noreply.github.com>
2025-04-23 20:43:34 +0200

    New testscripts

--- generic/Script2.sh
+++ generic/Script2.sh
@@ -24,47 +24,11 @@ serial_number=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 new_device_name="${first_name}-DTU-IT-${model_id}-${serial_number}"
 echo "📛 Setting system names to: $new_device_name"
 
-# Set all system name types
-scutil --set ComputerName "$new_device_name"
-scutil --set HostName "$new_device_name"
-scutil --set LocalHostName "$(echo "$new_device_name" | tr '[:upper:]' '[:lower:]' | tr -d ',')"
-
 # Create temporary command file
 command_file=$(mktemp /var/tmp/dialog_command.XXXXXX)
 chown "$logged_in_user" "$command_file"
 : >"$command_file" # Clear it
 
 sleep 4
-
-# Launch SwiftDialog with command file
-/usr/local/bin/dialog \
-    --title "Welcome to DTU, post-onboarding!" \
-    --message "DIALOG1" \
-    --icon SF=macbook \
-    --commandfile "$command_file" \
-    --timer 5 \
-    --hidetimerbar \
-    --quitoninfo \
-    --progress \
-    --moveable true \
-    --ontop \
-    --width 600 \
-    --height 350 & #Launch in background
-
-# Launch SwiftDialog with command file
-/usr/local/bin/dialog \
-    --title "DIALOG12 $first_name!" \
-    --message "We are done!." \
-    --icon SF=macbook \
-    --commandfile "$command_file" \
-    --timer 5 \
-    --hidetimerbar \
-    --quitoninfo \
-    --progress \
-    --moveable true \
-    --ontop \
-    --width 600 \
-    --height 350 & #Launch in background
-sleep 5
 
 echo "---- Script ended at $(date) ----"
